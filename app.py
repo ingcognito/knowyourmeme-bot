5602c0c865514f02d4b1eed2adc8ea0143095183
noah-ing <ing.noah@gmail.com>
2022-01-14 12:27:40 -0500

    start with basic requests

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = 'http://knowyourmeme.com/memes/pepehands'
+#url = 'https://knowyourmeme.com/'
+#headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Accept-Langage': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Connection': 'keep-alive'
+        }
+
+response = requests.get(url, headers=headers)
+print(response)
+print(response.text)
