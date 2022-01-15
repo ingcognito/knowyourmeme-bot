f7d5d2bb9f06d28db3e07b84a08bc5b45900d4cd
noah-ing <ing.noah@gmail.com>
2022-01-14 22:04:30 -0500

    search

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
-# from time import time
 from bs4 import BeautifulSoup
 
 _HEADERS = {
@@ -7,17 +6,14 @@ _HEADERS = {
 
 
 class KnowYourMeme:
-    def __init__(self, term):
+    def __init__(self):
         self.about = None
         self.imageurl = None
         self.siteurl = None
         self.soup = None
-        self.term = term
 
 
-    def search(self):
-#        start_time = time()  # set time on start
-        term = self.term
+    def search(self, term):
         for i in term:  # change every space to +
             if i == " ":
                 i = "+"
@@ -27,11 +23,8 @@ class KnowYourMeme:
         list1 = soup.findAll("a", href=True)  # Finding all links of search
         url2 = "http://knowyourmeme.com" + list1[129]['href']  # Picking first result and using its href
         page2 = requests.get(url2, headers=_HEADERS)  # requesting page again
-
         self.parse(page2.content)
 
-#        end_time = time()  # getting time on finish
-#        time = start_time - end_time  # time it took to do all this
 
 
     def random_image(self):
@@ -50,6 +43,6 @@ class KnowYourMeme:
         #return about['content']  # This is used for you to be able to print object and get definition print(nameofobject)
 
 if __name__ == '__main__':
-    kym = KnowYourMeme('pepehands') 
-    kym.search()
+    kym = KnowYourMeme() 
+    kym.search('asdf')
     kym.random_image()
